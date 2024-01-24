51d9f45c9675891c64fc6e058f540917510cee90
Julianbeton <julianibeton@iskolarngbayan.pup.edu.ph>
2024-01-24 20:35:23 +0800

    uploading function

--- calculate_income_tax_for_the_given_income_by_adhering_to_the_rules_below.py
+++ calculate_income_tax_for_the_given_income_by_adhering_to_the_rules_below.py
@@ -9,5 +9,16 @@
 # 10000*0% + 10000*10%  + 25000*20% = $6000.
 
 
-taxable_income = int(input("Enter a taxable income: "))
+taxable_income = int(input("\033[1;32;40mEnter a taxable income: "))
 print("taxable income is:", taxable_income)
+
+if taxable_income < 20000:
+   
+
+elif taxable_income == 20000:
+    income_tax = 10000/10
+    
+
+else: 
+    income_tax = ((taxable_income - 20000)/5)+1000
+  
